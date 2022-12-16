268f5024c823fc780186808c9e82e39aebad1eb1
Jos Vermoesen <info@rv.be>
2022-12-16 08:50:32 +0100

    End of section 6

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,82 @@
+import axios, { AxiosResponse } from 'axios';
+import { Activity } from '../models/activity';
+
+const sleep = (delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+};
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.response.use(
+  async (response) => {
+    try {
+      await sleep(1000);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return await Promise.reject(error);
+    }
+  }
+  /* (error) => {
+    const { data, status, config } = error.response;
+    switch (status) {
+      case 400:
+        if (typeof data === 'string') {
+          console.log(data);
+        }
+        if (config.method === 'get' && data.errors.hasOwnProperty('id')) {
+          history.push('/not-found');
+        }
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          console.log(data);
+        }
+        break;
+      case 401:
+        console.log(data);
+        break;
+      case 404:
+        history.push('/not-found');
+        break;
+      case 500:
+        store.commonStore.setServerError(data);
+        history.push('/server-error');
+        break;
+    } 
+        return Promise.reject(error);
+  } */
+);
+
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+const requests = {
+  get: <T>(url: string) => axios.get<T>(url).then(responseBody),
+  post: <T>(url: string, body: {}) =>
+    axios.post<T>(url, body).then(responseBody),
+  put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+};
+
+const Activities = {
+  list: () => requests.get<Activity[]>('/activities'),
+  details: (id: string) => requests.get(`/activities/${id}`),
+  create: (activity: Activity) => requests.post<void>('/activities', activity),
+  update: (activity: Activity) =>
+    requests.put<void>(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.del<void>(`/activities/${id}`),
+};
+
+const agent = {
+  Activities,
+};
+
+export default agent;
